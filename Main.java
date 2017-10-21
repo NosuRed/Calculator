a7b16beebd0360cd2ab810dc488b93b5d31bde34
NosuRed <31966656+NosuRed@users.noreply.github.com>
2017-10-21 16:41:39 +0200

    Add files via upload
    I change the data type from long to double. I also added a way to clear the calculator.

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("My first calculator");
         primaryStage.setScene(new Scene(root, 350, 450));
         primaryStage.setMinWidth(350);
         primaryStage.setMinHeight(450);
